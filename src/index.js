8ea2afae7f3fbebfdb54a271ef1f0ea29655edd0
AmharMahir <89715117+AmharMahir@users.noreply.github.com>
2022-07-28 09:00:52 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Assalamu Alikkum</h1>
+    <ul>
+      <li>teaching</li>
+      <li>coding</li>
+      <li>clerifiying the truth</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
